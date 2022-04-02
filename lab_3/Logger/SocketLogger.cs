3d450b7949da9f765e63056a076dcb1b441ae728
Andrzej.Kolacz <Andrzej.Kolacz@PK5-07>
2022-04-02 12:46:13 +0200

    Dodaj pliki projektów.

---
+++ lab_3/Logger/SocketLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_3
+{
+    public class SocketLogger : ILogger
+    {
+       public SocketLogger(string host, int port)
+        {
+            
+
+        }
+         ~SocketLogger()
+        {
+
+        }
+       public  void Log( string  message)
+        {
+
+        }
+        public void  Dispose()
+        {
+
+        }
+    }
+}
---
+++ lab_3/Logger/WriterLogger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace lab_3
+{
+    public abstract class WriterLogger : ILogger
+    {
+        protected TextWriter writer;
+        public virtual void Log(params string[] messages)
+        {
+            FileStream stream = new FileStream("plik-1.txt", FileMode.Append);
+
+            writer = new StreamWriter(stream, Encoding.UTF8);
+
+            // stream and writer should be closed and destroyed when not needed, eg.
+            //
+            //     writer.Close();
+            //     stream.Close();
+            //     writer.Dispose();
+            //     stream.Dispose();
+        }
+
+        // when we want to write text into console:
+        {
+            writer = Console.Out;
+
+            // writer is not created by programmer so we should not close or destroy it
+        }
+
+        writer.Write("Text to write here ...");
+        writer.Flush();
+        }
+
+        public abstract void Dispose();
+    }
+}
+
+
